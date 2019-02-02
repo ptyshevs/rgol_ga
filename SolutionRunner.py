b377afcdad54a7a15330dc032584f2261cb657a8
Paul Tyshevskyi <ptyshevs@gmail.com>
2019-02-02 19:39:59 +0200

    Add solution runner

---
+++ SolutionRunner.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+from MPGeneticSolver import MPGeneticSolver
+
+class SolutionRunner:
+  def __init__(self, save_fname='solution.csv', verbosity=0):
+    self.save_fname = save_fname
+    self.verbosity = verbosity
+    self.log = []
+    self.running_avg = 0
+    self.n = 0
+  
+  
+  def solve_df(self, df, save_to=None):
+    solver = MPGeneticSolver(early_stopping=False)
+    
+    solution_df = pd.DataFrame([], columns=['id', 'score'] + ['start.'+ str(_) for _ in range(1, 401)], dtype=int)
+    for col in solution_df.columns:
+      solution_df[col] = solution_df[col].astype(np.int32)
+    
+    self.running_avg = 0
+    self.n = 0
+    self.log = []
+    best, worst = None, None
+    for id, (idx, row) in zip(df.index, df.iterrows()):
+        delta, Y = row.values[0], row.values[1:].reshape((20, 20)).astype('uint8')
+        solution = mpgs.solve(Y, delta, return_all=False)
+
+        board, score = solution
+        flat_board = np.insert(board.ravel(), 0, id)
+        flat_board = np.insert(flat_board, 1, int(score * 100))
+        solution_df = solution_df.append(pd.Series(flat_board, index=solution_df.columns), ignore_index=True)
+
+        self.log.append((idx, score))
+        if best is None or best[1] < score:
+            best = (idx, score)
+        if worst is None or worst[1] > score:
+            worst = (idx, score)
+        n += 1
+        running_avg = (running_avg * (n - 1) + score) / n
+        if self.verbosity:
+          print(f"{idx} is solved with score {score}. Average score: {running_avg}")
+    if self.verbosity:
+      print("Best score:", best)
+      print("Worst score:", worst)
+    if save_to is not None:
+      solution_df.to_csv(save_to)
+    return solution_df
