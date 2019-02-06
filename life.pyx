76baa6327b098596b43b108b28882b78f8c828b9
Paul Tyshevskyi <ptyshevs@gmail.com>
2019-02-06 10:31:26 +0200

    Change make_move to work on board of any size

--- life.pyx
+++ life.pyx
@@ -5,30 +5,29 @@ import numpy as np
 @cython.boundscheck(False)
 @cython.nonecheck(False)
 @cython.wraparound(False)
-cdef int calc_neighs(unsigned char[:, :] field, int i, int j, int n):
+cdef int calc_neighs(unsigned char[:, :] field, int i, int j, int n, int k):
     cdef:
         int neighs = 0;
         int i_min = i - 1;
         int i_pl = i + 1;
         int j_min = j - 1;
         int j_pl = j + 1;
-        int k, row_idx, col_idx;
     neighs = 0
     if i_min >= 0:
         if j_min >= 0:
             neighs += field[i_min, j_min]
         neighs += field[i_min, j]
-        if j_pl < n:
+        if j_pl < k:
             neighs += field[i_min, j_pl]
     if j_min >= 0:
         neighs += field[i, j_min]
-    if j_pl < n:
+    if j_pl < k:
         neighs += field[i, j_pl]
-    if i + 1 < n:
+    if i_pl < n:
         if j_min >= 0:
             neighs += field[i_pl, j_min]
         neighs += field[i_pl, j]
-        if j_pl < n:
+        if j_pl < k:
             neighs += field[i_pl, j_pl]
     return neighs
 
@@ -39,18 +38,19 @@ cdef int calc_neighs(unsigned char[:, :] field, int i, int j, int n):
 cpdef make_move(unsigned char[:, :] field, int moves):
     cdef:
         int _, i, j, neighs;
-        int n;
+        int n, k;
         int switch = 0;
         unsigned char[:, :] cur_field;
         unsigned char[:, :] next_field;
     cur_field = np.copy(field)
     next_field = np.zeros_like(field, 'uint8')
-    n = len(field)
+    n = field.shape[0]
+    k = field.shape[1]
     for _ in range(moves):
         if switch == 0:
             for i in range(n):
-                for j in range(n):
-                    neighs = calc_neighs(cur_field, i, j, n)
+                for j in range(k):
+                    neighs = calc_neighs(cur_field, i, j, n, k)
                     if cur_field[i, j] and neighs == 2:
                         next_field[i, j] = 1
                     elif neighs == 3:
@@ -59,8 +59,8 @@ cpdef make_move(unsigned char[:, :] field, int moves):
                         next_field[i, j] = 0
         else:
             for i in range(n):
-                for j in range(n):
-                    neighs = calc_neighs(next_field, i, j, n)
+                for j in range(k):
+                    neighs = calc_neighs(next_field, i, j, n, k)
                     if next_field[i, j] and neighs == 2:
                         cur_field[i, j] = 1
                     elif neighs == 3:
